9efc55f21d73765750e080f39179a94eff50fb27
hitblunders <mouli.mondal145@gmail.com>
2021-02-06 17:43:09 -0500

    bot

--- frontend_ui/src/App.js
+++ frontend_ui/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import ChatWindow from './ChatWindow';
+import Home from './Home';
+import MoodSelection from './MoodSelection';
+import History from './History';
+
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route path="/mood" component={MoodSelection}/>
+              <Route path="/chat" component={ChatWindow}/>
+              <Route path="/history" component={History}/>
+          </Switch>
+      </Router>
   );
 }
 
